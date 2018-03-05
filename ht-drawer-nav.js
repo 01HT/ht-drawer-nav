9d52f9c8d08bfceede1dfcae2bd95c9691fb1cbb
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-03-05 23:06:40 +0300

    Update ht-drawer-nav.js

--- ht-drawer-nav.js
+++ ht-drawer-nav.js
@@ -1,13 +1,15 @@
 "use strict";
-import { Element } from "../@polymer/polymer/polymer-element.js";
+import { LitElement, html } from "../@polymer/lit-element/lit-element.js";
+import { repeat } from "../lit-html/lib/repeat.js";
 import "../@polymer/iron-iconset-svg/iron-iconset-svg.js";
+import "../@polymer/paper-item/paper-item.js";
 import "../@polymer/paper-item/paper-icon-item.js";
 import "../@polymer/iron-icon/iron-icon.js";
 import "../@polymer/iron-selector/iron-selector.js";
-class HTDrawerNav extends Element {
-  static get template() {
-    return `
-      <style>
+
+class HTDrawerNav extends LitElement {
+  render({ data, page }) {
+    return html`<style>
         :host {
             display: block;
             position: relative;
@@ -20,18 +22,22 @@ class HTDrawerNav extends Element {
             outline: none;
         }
 
-        a.iron-selected paper-icon-item {
+        a.iron-selected paper-icon-item, a.iron-selected paper-item {
           color: var(--accent-color);
         }
 
-        paper-icon-item {
+        paper-item, paper-icon-item {
           color: var(--secondary-text-color);
         }
 
-        paper-icon-item {
+        paper-item, paper-icon-item {
             --paper-item-focused-before: {
                 background: none;
             }
+        }
+
+        paper-item {
+          padding-left: 24px;
         }
       </style>
       <iron-iconset-svg size="24" name="ht-drawer-nav">
@@ -39,41 +45,54 @@ class HTDrawerNav extends Element {
               <defs id="defs"></defs>
           </svg>
       </iron-iconset-svg>
-      <iron-selector attr-for-selected="name" selected="[[page]]" selectable="a">
-        <dom-repeat items="[[data]]" as="item">
-          <template>
-            <a name="[[item.name]]" href="/[[item.name]]">
-              <paper-icon-item>
-                  <iron-icon icon="ht-drawer-nav:[[item.name]]" item-icon slot="item-icon"></iron-icon>
-                  <span>[[item.title]]</span>
-              </paper-icon-item>
+      <iron-selector attr-for-selected="name" selected$=${page} selectable="a">
+         ${repeat(
+           data,
+           i => html`
+            <a name="${i.name}" href="/${i.name}">
+              ${
+                i.icon
+                  ? html`<paper-icon-item>
+                  <iron-icon icon="ht-drawer-nav:${
+                    i.name
+                  }" item-icon slot="item-icon"></iron-icon>
+                  <span>${i.title}</span>
+              </paper-icon-item>`
+                  : html`<paper-item>
+                  ${i.title}
+              </paper-item>`
+              }
             </a>
-          </template>
-        </dom-repeat>
+          `
+         )}
       </iron-selector>
-`;
+    `;
   }
+
   static get is() {
     return "ht-drawer-nav";
   }
 
   static get properties() {
     return {
-      data: {
-        type: Array,
-        value: _ => []
-      },
-
-      page: {
-        type: String,
-        value: ""
-      }
+      data: Array,
+      page: String
     };
+  }
+
+  constructor() {
+    super();
+    this.data = [
+      { name: "catalog", title: "Каталог" },
+      { name: "about", title: "О сервисе" },
+      { name: "author", title: "Стать автором" }
+    ];
   }
 
   ready() {
     super.ready();
     for (let i of this.data) {
+      if (i.icon === undefined) return;
       this.$.defs.innerHTML += `<g id="${i.name}"><path d="${
         i.icon
       }"></path></g>`;
